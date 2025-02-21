7e5bd91ccd23aef5213e1ede5a68b3bc1af1fe03
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-22 00:24:25 +0530

    connection to monogodb

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+import { PASSWORD, USERNAME } from "@/utils/constants";
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+  const url = `mongodb+srv://${USERNAME}:${PASSWORD}@usermanagementapp.xodvl.mongodb.net/`;
+
+  mongoose
+    .connect(url)
+    .then(() => console.log("DataBase Connected Sucessfully"))
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export default connectToDB;
