90ba6c397c83725358ed9475e5c63d90ed42f0b6
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-25 15:11:47 +0530

    practice

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import { PASSWORD, USERNAME } from "@/utils/constants";
 import mongoose from "mongoose";
 
 const connectToDB = async () => {
-  const url = `mongodb+srv://${USERNAME}:${PASSWORD}@user-management-app.zm8h0.mongodb.net/`;
+  const url = `mongodb+srv://${USERNAME}:${PASSWORD}@usermanagementapp.l1x2t.mongodb.net/`;
   mongoose
     .connect(url)
     .then(() => console.log("DataBase Connected Sucessfully"))
--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,13 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    address: { type: String, required: true, trim: true },
-  },
-  { timestamps: true }
-);
+const UserSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  address: String,
+});
 
-const User = mongoose.models.User || mongoose.model("User", UserSchema);
+// Ensure mongoose.models is properly initialized before accessing it
+const User = mongoose.model("User", UserSchema) || mongoose.models.User;
+
 export default User;
