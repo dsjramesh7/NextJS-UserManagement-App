634ebb129730fabca975de7909fab67c65eeea80
dsjramesh7 <rameshdussa07@gmail.com>
2025-03-04 00:19:33 +0530

    practice

--- src/app/user-management/page.js
+++ src/app/user-management/page.js
@@ -1,15 +1,33 @@
 import { fetchUserAction } from "@/actions";
 import AddNewUser from "@/components/AddNewUser";
+import UserCard from "@/components/UserCard";
 
 const UserManagementPage = async () => {
-  const listOfAllUsers = await fetchUserAction();
-  console.log(listOfAllUsers);
+  const getListOfAllUsers = await fetchUserAction();
+  console.log(getListOfAllUsers);
   return (
     <div className="bg-black min-h-screen text-white">
       <div className="p-20 max-w-6xl mx-auto">
         <div className="flex justify-between">
           <h1 className="">User Management</h1>
           <AddNewUser />
+        </div>
+        <div className="">
+          {getListOfAllUsers &&
+          getListOfAllUsers?.data &&
+          getListOfAllUsers?.data?.length > 0 ? (
+            getListOfAllUsers.data.map((singleUser) => (
+              <UserCard
+                key={singleUser._id}
+                firstName={singleUser.firstName}
+                lastName={singleUser.lastName}
+                email={singleUser.email}
+                address={singleUser.address}
+              />
+            ))
+          ) : (
+            <p>No Users Found Create One</p>
+          )}
         </div>
       </div>
     </div>
--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -3,24 +3,26 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Pencil, Trash } from "lucide-react";
 
-const UserCard = ({ id, name, email, role, onEdit, onDelete }) => {
+const UserCard = ({ firstName, email, lastName, address }) => {
   return (
     <Card className="w-full max-w-sm p-4 rounded-2xl shadow-lg bg-white dark:bg-gray-900">
       <CardHeader>
-        <CardTitle className="text-xl font-semibold">{name}</CardTitle>
+        <CardTitle className="text-xl font-semibold">
+          {firstName} {lastName}
+        </CardTitle>
       </CardHeader>
       <CardContent className="space-y-2">
         <p className="text-gray-600 dark:text-gray-300">
           <strong>Email:</strong> {email}
         </p>
         <p className="text-gray-600 dark:text-gray-300">
-          <strong>Role:</strong> {role}
+          <strong>Address:</strong> {address}
         </p>
         <div className="flex justify-end gap-2 mt-4">
-          <Button variant="outline" size="sm" onClick={() => onEdit(id)}>
+          <Button variant="outline" size="sm">
             <Pencil className="w-4 h-4 mr-2" /> Edit
           </Button>
-          <Button variant="destructive" size="sm" onClick={() => onDelete(id)}>
+          <Button variant="destructive" size="sm">
             <Trash className="w-4 h-4 mr-2" /> Delete
           </Button>
         </div>
